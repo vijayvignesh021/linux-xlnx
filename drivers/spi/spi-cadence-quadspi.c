torvalds/linux

commit 0f3841a5e1152eca1a58cfbd9ceb6d311aa7e647

spi: cadence-qspi: report correct number of chip-select
Set the ->num_chipselect field in struct cqspi_st and struct
spi_controller to the current number of chip-select. The value is
dependent on declared flashes in devicetree.

Previously, the num-cs property from devicetree or the maximum value was
being reported.

Signed-off-by: Théo Lebrun <theo.lebrun@bootlin.com>
Link: https://msgid.link/r/20240209-cdns-qspi-cs-v1-3-a4f9dfed9ab4@bootlin.com
Signed-off-by: Mark Brown <broonie@kernel.org>

drivers/spi/spi-cadence-quadspi.c
@@ -1621,6 +1621,7 @@ static const struct spi_controller_mem_caps cqspi_mem_caps = {
 
 static int cqspi_setup_flash(struct cqspi_st *cqspi)
 {
+	unsigned int max_cs = cqspi->num_chipselect - 1;
 	struct platform_device *pdev = cqspi->pdev;
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
@@ -1641,6 +1642,8 @@ static int cqspi_setup_flash(struct cqspi_st *cqspi)
 			dev_err(dev, "Chip select %d out of range.\n", cs);
 			of_node_put(np);
 			return -EINVAL;
+		} else if (cs < max_cs) {
+			max_cs = cs;
 		}
 
 		f_pdata = &cqspi->f_pdata[cs];
@@ -1654,6 +1657,7 @@ static int cqspi_setup_flash(struct cqspi_st *cqspi)
 		}
 	}
 
+	cqspi->num_chipselect = max_cs + 1;
 	return 0;
 }
 
@@ -1864,13 +1868,13 @@ static int cqspi_probe(struct platform_device *pdev)
 	cqspi->current_cs = -1;
 	cqspi->sclk = 0;
 
-	host->num_chipselect = cqspi->num_chipselect;
-
 	ret = cqspi_setup_flash(cqspi);
 	if (ret) {
 		dev_err(dev, "failed to setup flash parameters %d\n", ret);
 		goto probe_setup_failed;
 	}
+
+	host->num_chipselect = cqspi->num_chipselect;
 
 	if (cqspi->use_direct_mode) {
 		ret = cqspi_request_mmap_dma(cqspi);

vijayvignesh021/linux-xlnx

commit c912f71017fac118be995bd307427e929e99d4e6

spi: cadence-quadspi: Retrieve all chip-select values from the flash node
In stacked mode, the driver does not account for the second chip-select
value when setting the ->num_chipselect field in the struct cqspi_st and
struct spi_controller to the current chip-select count in commit
0f3841a5e115 ("spi: cadence-qspi: report correct number of chip-select"),
leading to a probe failure. Update the driver to retrieve all chip-select
values from the flash node.

Fixes: 0f3841a5e115 ("spi: cadence-qspi: report correct number of chip-select")
Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@amd.com>
State: pending
Message-ID: <20250101183721.2869197-1-amit.kumar-mahapatra@amd.com>

drivers/spi/spi-cadence-quadspi.c
@@ -33,6 +33,7 @@
 
 #define CQSPI_NAME			"cadence-qspi"
 #define CQSPI_MAX_CHIPSELECT		4
+#define CQSPI_MIN_CHIPSELECT		1
 
 static_assert(CQSPI_MAX_CHIPSELECT <= SPI_CS_CNT_MAX);
 
@@ -2094,28 +2095,38 @@ static int cqspi_setup_flash(struct cqspi_st *cqspi)
 	struct platform_device *pdev = cqspi->pdev;
 	struct device *dev = &pdev->dev;
 	struct cqspi_flash_pdata *f_pdata;
+	unsigned int cs_num[2] = {0};
 	unsigned int cs;
 	int ret;
 
 	/* Get flash device data */
 	for_each_available_child_of_node_scoped(dev->of_node, np) {
-		ret = of_property_read_u32(np, "reg", &cs);
-		if (ret) {
+		ret = of_property_read_variable_u32_array(np, "reg", &cs_num[0],
+							  CQSPI_MIN_CHIPSELECT,
+							  CQSPI_MAX_CHIPSELECT);
+		if (ret < 0) {
 			dev_err(dev, "Couldn't determine chip select.\n");
 			return ret;
 		}
+		cs = cs_num[0];
 
+		f_pdata = &cqspi->f_pdata[cs];
+		f_pdata->cqspi = cqspi;
+		f_pdata->cs = cs;
+
+		if (ret > CQSPI_MIN_CHIPSELECT) {
+			/* Obtain the maximum CS value between the two CS values */
+			for (int i = CQSPI_MIN_CHIPSELECT; i < ret; i++) {
+				if (cs < cs_num[i])
+					cs = cs_num[i];
+			}
+		}
 		if (cs >= cqspi->num_chipselect) {
 			dev_err(dev, "Chip select %d out of range.\n", cs);
 			return -EINVAL;
 		} else if (cs < max_cs) {
 			max_cs = cs;
 		}
-
-		f_pdata = &cqspi->f_pdata[cs];
-		f_pdata->cqspi = cqspi;
-		f_pdata->cs = cs;
-
 		ret = cqspi_of_get_flash_pdata(pdev, f_pdata, np);
 		if (ret)
 			return ret;